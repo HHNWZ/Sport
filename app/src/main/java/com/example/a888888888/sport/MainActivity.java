55acbd36321f8403da1be1c27430b96f48656b98
NNEECCOO <neconeco018@gmail.com>
2017-10-20 16:27:14 +0800

    改了INT

--- app/src/main/java/com/example/a888888888/sport/MainActivity.java
+++ app/src/main/java/com/example/a888888888/sport/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.a888888888.sport;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a;
+        int 幹=0;
+        幹=幹+1;
     }
 }
