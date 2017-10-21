15917a917b86f8065afeecfc3f5494b66613da8c
kelvinngshuhui <kelvinngshuhui@gmail.com>
2017-10-21 22:33:31 +0800

    改變kelvin.tablayout

--- app/src/main/java/kelvin/tablayout/kelvin_tab_layout.java
+++ app/src/main/java/kelvin/tablayout/kelvin_tab_layout.java
@@ -2,14 +2,19 @@ package kelvin.tablayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.a888888888.sport.R;
 
 public class kelvin_tab_layout extends AppCompatActivity {
 
+    Toolbar toolbar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kelvin_tab_layout);
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
     }
 }
