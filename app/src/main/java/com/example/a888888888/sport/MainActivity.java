83a78b50225d813d5e8326b600a12b389463ba9a
Billy He <x7530300x@gmail.com>
2017-10-22 15:27:50 +0800

    yee

--- app/src/main/java/com/example/a888888888/sport/MainActivity.java
+++ app/src/main/java/com/example/a888888888/sport/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 import kelvin.tablayout.kelvin_tab_layout;
+import necowneco.tablayout.habaActivity;
 import qwer.Dietcontrol;
 
 public class  MainActivity extends AppCompatActivity
@@ -20,7 +21,7 @@ public class  MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button kel = (Button)findViewById(R.id.button); //連至書輝的按鈕
-        Button hel = (Button)findViewById(R.id.button1); //連至弘盛的按鈕
+        Button hal = (Button)findViewById(R.id.button1); //連至弘盛的按鈕
         Button del = (Button)findViewById(R.id.button2); //連至琨城的按鈕
         Button over = (Button)findViewById(R.id.button3); //連至直播的按鈕
         Button sport = (Button)findViewById(R.id.button4); //連至運動的按鈕
@@ -29,6 +30,15 @@ public class  MainActivity extends AppCompatActivity
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this,kelvin_tab_layout.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+        hal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,habaActivity.class);
                 startActivity(intent);
                 MainActivity.this.finish();
             }
