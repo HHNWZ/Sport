9db779de753d831e67a216ad7ed877fac1a97c20
Billy He <x7530300x@gmail.com>
2017-10-22 15:21:42 +0800

    yee

--- app/src/main/java/qwer/Dietcontrol.java
+++ app/src/main/java/qwer/Dietcontrol.java
@@ -3,12 +3,14 @@ package qwer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.a888888888.sport.R;
 
-public class Dietcontrol extends Fragment {
+public class Dietcontrol extends Fragment implements View.OnTouchListener {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +20,14 @@ public class Dietcontrol extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
+        View view = inflater.inflate(R.layout.qwer_dietcontrol, null);
+        view.setOnTouchListener(this);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.qwer_dietcontrol, container, false);
+        return view;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
 }
