85e277ac408b90cc16301f8fac55d7988eeee7ae
NNEECCOO <neconeco018@gmail.com>
2017-10-25 17:34:05 +0800

    頁面:文字敘述

--- app/src/main/java/kelvin/tablayout/SitUpsFragment.java
+++ app/src/main/java/kelvin/tablayout/SitUpsFragment.java
@@ -24,7 +24,7 @@ public class SitUpsFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    // TODO: Rename andchange types of parameters
     private String mParam1;
     private String mParam2;
 
