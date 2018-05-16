ed6e4e24d551347fde772efa524beff5ce3de9a1
x7530300x <x7530300x@gmail.com>
2018-05-16 14:04:21 +0800

    登入成功

--- app/src/main/java/com/example/a888888888/sport/Register.java
+++ app/src/main/java/com/example/a888888888/sport/Register.java
@@ -20,36 +20,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-
-import cz.msebera.android.httpclient.HttpEntity;
-import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.HttpStatus;
-import cz.msebera.android.httpclient.NameValuePair;
-import cz.msebera.android.httpclient.client.ClientProtocolException;
-import cz.msebera.android.httpclient.client.CookieStore;
-import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
-import cz.msebera.android.httpclient.client.methods.HttpPost;
-import cz.msebera.android.httpclient.cookie.Cookie;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
-import cz.msebera.android.httpclient.protocol.HTTP;
-import cz.msebera.android.httpclient.util.EntityUtils;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Login.OnFragmentInteractionListener} interface
+ * {@link Register.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Login#newInstance} factory method to
+ * Use the {@link Register#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Login extends Fragment {
+public class Register extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -61,7 +44,7 @@ public class Login extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Login() {
+    public Register() {
         // Required empty public constructor
     }
 
@@ -71,11 +54,11 @@ public class Login extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Login.
+     * @return A new instance of fragment Register.
      */
     // TODO: Rename and change types and number of parameters
-    public static Login newInstance(String param1, String param2) {
-        Login fragment = new Login();
+    public static Register newInstance(String param1, String param2) {
+        Register fragment = new Register();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -91,17 +74,12 @@ public class Login extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-    private EditText login1,login2;
-    private String showUri = "http://172.30.4.170:1335/login.php";
-    private HttpPost myPost;
-    private DefaultHttpClient httpClient;
-    private HttpEntity httpEntity;
-    private HttpResponse httpResponse;
-    public static String user = null;
-    public static String PHPSESSID = null;
-    public static String userimage;
+
+    private Button reg;
+    private EditText account, password;
     com.android.volley.RequestQueue requestQueue;
-    private void getData() {
+    private String showUri = "http://172.30.4.170:1335/register.php";
+    private void upData() {
 
         StringRequest jsonObjectRequest = new StringRequest
                 (Request.Method.POST,showUri, new Response.Listener<String>() {
@@ -111,28 +89,16 @@ public class Login extends Fragment {
                             JSONObject jsonObject= new JSONObject(response.toString());
                             JSONArray data = jsonObject.getJSONArray("data");
                             JSONObject jasondata;
+                            jasondata = data.getJSONObject(0);
+                            if(jasondata.getString("data")=="1")
+                            {
+                                Toast.makeText(getActivity(), "註冊成功", Toast.LENGTH_SHORT).show();//傳資料回來在這裡
+                            }
                             //這邊要和上面json的名稱一樣
                             //下邊是把全部資料都印出來
-                            for (int i = 0; i < data.length(); i++) {
-                                jasondata = data.getJSONObject(i);
-                                if(Objects.equals(login1.getText().toString(), jasondata.getString("account")) &&
-                                    Objects.equals(login2.getText().toString(), jasondata.getString("password")))
-                                {
-                                    userimage = jasondata.getString("img");
-                                    user = jasondata.getString("name");
-                                    Toast.makeText(getActivity(), "登入成功", Toast.LENGTH_SHORT).show();
-                                    getActivity().onBackPressed();
-
-                                }
-                                else
-                                {   login1.setText("");
-                                    login2.setText("");
-                                    Toast.makeText(getActivity(), "登入失敗，請重新輸入", Toast.LENGTH_SHORT).show();}
-                            }
-
                         } catch (JSONException e) {
                             e.printStackTrace();
-                            Toast.makeText(getActivity(), "登入失敗，請重新輸入", Toast.LENGTH_SHORT).show();//傳資料回來在這裡
+                            Toast.makeText(getActivity(), "註冊失敗，請重新輸入", Toast.LENGTH_SHORT).show();//傳資料回來在這裡
                         }
                     }
                 }, new Response.ErrorListener() {
@@ -148,70 +114,32 @@ public class Login extends Fragment {
             protected Map<String, String> getParams()
             {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("login", login1.getText().toString());
+                params.put("account",account.getText().toString());
+                params.put("password",password.getText().toString());
                 return params;
             }
 
         };
         requestQueue.add(jsonObjectRequest);
     }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
         // Inflate the layout for this fragment
-        login1 = (EditText)view.findViewById(R.id.editText);
-        login2 = (EditText)view.findViewById(R.id.editText2);
-        Button login = (Button) view.findViewById(R.id.button18);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        reg = view.findViewById(R.id.button18);
+        account = view.findViewById(R.id.editText1);
+        password = view.findViewById(R.id.editText2);
         requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
-        login.setOnClickListener(new View.OnClickListener() {
+        reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getData();
+                upData();
             }
         });
-        
         return view;
-    }
-
-    public String executeRequest( List<NameValuePair> params) { //session設定，未成功，之後再優化
-        String ret = "none";
-        try {
-            myPost = new HttpPost("http://172.30.4.170:1335/data_save.php");
-            httpEntity = new UrlEncodedFormEntity(params, HTTP.UTF_8);
-            myPost.setEntity(httpEntity);
-//第一次一般是还未被赋值，若有值则将SessionId发给服务器
-            if(null != PHPSESSID){
-                myPost.setHeader("Cookie", "PHPSESSID=" + PHPSESSID);
-            }
-            httpClient = new DefaultHttpClient();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
-            httpResponse = httpClient.execute(myPost);
-            if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                HttpEntity entity = httpResponse.getEntity();
-                ret = EntityUtils.toString(entity);
-                CookieStore mCookieStore = httpClient.getCookieStore();
-                List<Cookie> cookies = mCookieStore.getCookies();
-                for (int i = 0; i < cookies.size(); i++) {
-//这里是读取Cookie['PHPSESSID']的值存在静态变量中，保证每次都是同一个值
-                    if ("PHPSESSID".equals(cookies.get(i).getName())) {
-                        PHPSESSID = cookies.get(i).getValue();
-                        break;
-                    }
-
-                }
-
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getActivity(), "1 "+PHPSESSID, Toast.LENGTH_SHORT).show();
-        return ret;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -253,4 +181,3 @@ public class Login extends Fragment {
         void onFragmentInteraction(String Tag, String number);
     }
 }
-
