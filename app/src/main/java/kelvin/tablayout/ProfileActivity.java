2b1c5606e5427373ec75f45091973277abfc1b5f
nobodyaa3 <kelvinngshuhui2016@gmail.com>
2018-07-29 21:12:31 +0800

    2018/7/29 幫profile.java加了Toolbar

--- app/src/main/java/kelvin/tablayout/ProfileActivity.java
+++ app/src/main/java/kelvin/tablayout/ProfileActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.a888888888.sport.MainActivity;
 import com.example.a888888888.sport.R;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -76,7 +77,7 @@ public class ProfileActivity extends AppCompatActivity {
         profile_app_bar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(ProfileActivity.this,ChatActivity.class);
+                Intent intent =new Intent(ProfileActivity.this,MainActivityFireBase.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
