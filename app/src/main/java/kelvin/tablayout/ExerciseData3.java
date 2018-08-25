89d61a127fbead419dc930bf32bae14f3b754acf
nobodyaa3 <kelvinngshuhui2016@gmail.com>
2018-08-25 23:49:04 +0800

    2018/8/25 23:48 修改

--- app/src/main/java/kelvin/tablayout/ExerciseData3.java
+++ app/src/main/java/kelvin/tablayout/ExerciseData3.java
@@ -21,8 +21,61 @@ public class ExerciseData3 {
         this.calorie=calorie;
         this.max_heart_rate=max_heart_rate;
         this.count=count;
+    }
 
+    public String getStart_time() {
+        return start_time;
+    }
 
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
 
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public int getMean_heart_rate() {
+        return mean_heart_rate;
+    }
+
+    public void setMean_heart_rate(int mean_heart_rate) {
+        this.mean_heart_rate = mean_heart_rate;
+    }
+
+    public int getCalorie() {
+        return calorie;
+    }
+
+    public void setCalorie(int calorie) {
+        this.calorie = calorie;
+    }
+
+    public int getMax_heart_rate() {
+        return max_heart_rate;
+    }
+
+    public void setMax_heart_rate(int max_heart_rate) {
+        this.max_heart_rate = max_heart_rate;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }
