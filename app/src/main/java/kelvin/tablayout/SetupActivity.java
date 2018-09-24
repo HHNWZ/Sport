635c5cad38bf2d6899790a0bb030be5812978cd6
nobodyaa3 <kelvinngshuhui2016@gmail.com>
2018-09-24 13:31:38 +0800

    2018/09/24 08:00 跟新

--- app/src/main/java/kelvin/tablayout/SetupActivity.java
+++ app/src/main/java/kelvin/tablayout/SetupActivity.java
@@ -10,6 +10,6 @@ public class SetupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup);//1235
+        setContentView(R.layout.activity_setup);//1236
     }
 }
